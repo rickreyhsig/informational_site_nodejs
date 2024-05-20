a0981d75533133ee743dd9b8755f68e8b733eb93
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2024-05-20 18:24:01 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('node:http');
+const fs = require('fs');
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+    // Routing logic
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else {
+    // Handle other routes
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Page Not Found');
+  }
+//   res.writeHead(200, { 'Content-Type': 'application/json' });
+//   res.end(JSON.stringify({
+//     data: 'Hello World!',
+//   }));
+});
+
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+//server.listen(8080);
